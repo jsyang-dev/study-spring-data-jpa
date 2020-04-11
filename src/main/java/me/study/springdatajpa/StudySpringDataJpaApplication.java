da2028002a58699aef515cdc2ef294f6bb213f2a
Jeongsu Yang <mycat83@gmail.com>
2020-04-11 22:04:44 +0900

    Initial commit

---
+++ src/main/java/me/study/springdatajpa/StudySpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package me.study.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringDataJpaApplication.class, args);
+    }
+
+}
