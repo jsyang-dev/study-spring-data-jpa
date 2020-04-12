437b09c2e3e47bc2eed1d26db933b618a297aa10
Jeongsu Yang <mycat83@gmail.com>
2020-04-12 21:37:18 +0900

    스프링 데이터 JPA와 DB 설정, 동작확인

---
+++ src/test/java/me/study/springdatajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,30 @@
+package me.study.springdatajpa.repository;
+
+import me.study.springdatajpa.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("MemberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(member.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
