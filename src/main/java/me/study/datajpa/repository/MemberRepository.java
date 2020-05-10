59ee5660a9f1fa9aac54201804f9cbe2591d8c44
Jeongsu Yang <mycat83@gmail.com>
2020-05-10 22:05:09 +0900

    네이티브 쿼리 #1

--- src/main/java/me/study/datajpa/repository/MemberRepository.java
+++ src/main/java/me/study/datajpa/repository/MemberRepository.java
@@ -76,4 +76,7 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     List<UsernameOnly> findProjectionsByUsername(@Param("username") String username);
     List<UsernameOnlyDto> findProjectionsDtoByUsername(@Param("username") String username);
     <T> List<T> findProjectionsDynamicByUsername(@Param("username") String username, Class<T> type);
+
+    @Query(value = "select * from member where username = ?", nativeQuery = true)
+    Member findByNativeQuery(String username);
 }
